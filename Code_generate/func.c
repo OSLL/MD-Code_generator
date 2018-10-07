98151613b54e42240616fc352f44b1e49c7e13d7
AigulKh <aigul.kh017@gmail.com>
2018-10-07 21:31:44 +0300

    delete .idea

---
+++ Code_generate/func.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+
+double addition(float c, double a) {
+    b = c;
+    a = b;
+}
