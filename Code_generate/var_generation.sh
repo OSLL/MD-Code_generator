1b3f9c7a89236237ae4d0ae281ce66b0687829d1
AigulKh <aigul.kh017@gmail.com>
2019-03-14 02:03:16 +0300

    update script for generating variants

--- Code_generate/var_generation.sh
+++ Code_generate/var_generation.sh
@@ -11,27 +11,27 @@ do
     echo variant_${randSeed} > variants/answer${randSeed}.docx
     
     echo | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
-    echo "1 $randSeed 2 4 2 1 1 * - +" >> variants/answer${randSeed}.docx
+    echo "1 $randSeed 2 4 2 1 1 * +" >> variants/answer${randSeed}.docx
     echo >> variants/answer${randSeed}.docx
-    java -jar generate.jar "1 $randSeed 2 4 2 1 1 * - +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
+    java -jar generate.jar "1 $randSeed 2 4 2 1 1 * +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
     gcc func.c -o func
     echo >> variants/answer${randSeed}.docx
     ./func >> variants/answer${randSeed}.docx
     echo "______________________________________"| tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
     
     echo | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
-    echo "1 $randSeed 4 8 4 2 0 | & * - +" >> variants/answer${randSeed}.docx
+    echo "1 $randSeed 4 8 4 2 0 | & * +" >> variants/answer${randSeed}.docx
     echo >> variants/answer${randSeed}.docx
-    java -jar generate.jar "1 $randSeed 4 8 4 2 0 | & * - +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
+    java -jar generate.jar "1 $randSeed 4 8 4 2 0 | & * +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
     gcc func.c -o func
     echo >> variants/answer${randSeed}.docx
     ./func >> variants/answer${randSeed}.docx
     echo "______________________________________"| tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
     
     echo | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
-    echo "1 $randSeed 6 12 7 3 0 >> << | & * - +" >> variants/answer${randSeed}.docx
+    echo "1 $randSeed 2 12 7 3 0 >> << | & * +" >> variants/answer${randSeed}.docx
     echo >> variants/answer${randSeed}.docx
-    java -jar generate.jar "1 $randSeed 6 12 7 3 0 >> << | & * - +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
+    java -jar generate.jar "1 $randSeed 2 12 7 3 0 >> << | & * +" | tee -a variants/var${randSeed}.docx >> variants/answer${randSeed}.docx
     gcc func.c -o func
     echo >> variants/answer${randSeed}.docx
     ./func >> variants/answer${randSeed}.docx
