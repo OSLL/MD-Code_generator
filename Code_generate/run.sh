da7ebc800101bb375818500564ed72caadade9ef
AigulKh <aigul.kh017@gmail.com>
2019-05-20 18:05:17 +0300

    update bash script

--- Code_generate/run.sh
+++ Code_generate/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 kotlinc src/generate.kt -include-runtime -d generate.jar
-#java -jar generate.jar '1 19 1 10 3 4 1 << >> | & * +'
-#java -jar generate.jar '2 48'
-java -jar generate.jar '3 15'
-#java -jar generate.jar '4 5'
+#java -jar generate.jar '1 19 1 4 3 2 1 << >> | & * +'
+#java -jar generate.jar '2 48 2 4 1' #if
+#java -jar generate.jar '3 48 3 4 0' #switch
+#java -jar generate.jar '4 15 3 4 1' #while
+#java -jar generate.jar '5 15 3 4 1' #dowhile
+java -jar generate.jar '6 5 3 2 1' #for
 gcc func.c -o func
 ./func
