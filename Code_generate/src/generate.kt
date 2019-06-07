2f6ef24b18456cf927767d9355e9e8f7a8092843
AigulKh <aigul.kh017@gmail.com>
2019-06-07 19:58:41 +0300

    added server class

--- Code_generate/src/generate.kt
+++ Code_generate/src/generate.kt
@@ -1,12 +1,5 @@
 package com.example
-/*import io.ktor.application.call
-import io.ktor.response.respondText
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty*/
 
-import kotlinx.html.RunAt
 import java.io.File
 import java.lang.Integer.parseInt
 import java.util.*
