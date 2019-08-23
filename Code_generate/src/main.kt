fe53d7f8b397747f036db6be6079a9e42cfc99af
AigulKh <aigul.kh017@gmail.com>
2019-08-23 03:05:05 +0300

    refactoring system

---
+++ Code_generate/src/main.kt
@@ -0,0 +1,26 @@
+import com.example.Server
+/*
+import com.example.Generator
+import com.example.ProgramParameters
+import java.lang.Integer.parseInt
+
+val ERROR_TEXT = "Ошибка ввода. Попробуйте снова.\n\n"
+*/
+
+fun main(args: Array<String>) {
+    val server = Server()
+/*
+    val args_: MutableList<String> = mutableListOf()
+    args_.addAll(args[0].split(' '))
+    if (!args_.isEmpty()) {
+        if ((parseInt(args_[0]) == 1 && args_.size >= 8) || (parseInt(args_[0]) == 6 && args_.size == 7)
+            || ((parseInt(args_[0]) == 2 || parseInt(args_[0]) == 4 || parseInt(args_[0]) == 5) && args_.size == 6)) {
+            val parameters = ProgramParameters(args_)
+            val generator = Generator(parameters)
+            generator.programGenerate()
+        }
+        else println(ERROR_TEXT)
+    }
+    else println(ERROR_TEXT)
+*/
+}
---
+++ Code_generate/src/terminal_alphabet.kt
@@ -0,0 +1,54 @@
+package com.example
+
+const val MAX_VALUE: Int = 7
+
+val BOOL = "bool"
+val INT = "int"
+val FLOAT = "float"
+val SIZE_T = "size_t"
+val UNSIGNED_INT = "unsigned int"
+val MAIN = "main"
+val ARITHMETIC_OPERATIONS: List<String> = listOf("+", "-", "*", "/")
+val ADDITION = "+"
+val SUBTRACTION = "-"
+val MOD = "%"
+val DIV = "/"
+val MULTIPLICATION = "*"
+val LOGICAL_NEGATION = "!"
+val LOGICAL_AND = "&&"
+val LOGICAL_OR = "||"
+val LOGICAL_OPERATIONS: List<String> = listOf("!", "&&", "||")
+val RELATIONAL_OPERATIONS: List<String> = listOf("<", ">", "<=", ">=", "==", "!=")
+val SPECIAL_OPERATIONS: List<String> = listOf("++", "--")
+val BITWISE_OPERATIONS: List<String> = listOf("<<", ">>", "|", "&")
+val IDENTIFIER: List<String> = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+val CARRIAGE_RETURN = "\n"
+val PRINT_CARRIAGE_RETURN = "\\n"
+val EQUALLY = "="
+val COMMA = ","
+val DOT = "."
+val DASH = "-"
+val SEMICOLON = ";"
+val COLON = ":"
+val QUOTES = "\""
+val END_OF_LINE = "$SEMICOLON$CARRIAGE_RETURN"
+val LIBRARY: List<String> = listOf("stdio.h", "stdbool.h")
+val INCLUDE: List<String> = listOf("#include <", ">")
+val ROUND_BRACKET = "("
+val ROUND_BRACKET_ = ")"
+val BRACE = "{"
+val BRACE_ = "}"
+val SQUARE_BRACKET = "["
+val SQUARE_BRACKET_ = "]"
+val BREAK = "break"
+val CONTINUE = "continue"
+val RETURN = "return"
+val PRINTF = "printf"
+val IF = "if"
+val ELSE = "else"
+val SWITCH = "switch"
+val CASE = "case"
+val DEFAULT = "default"
+val WHILE = "while"
+val DO = "do"
+val FOR = "for"
