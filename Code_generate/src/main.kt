0136ebab3cfa0c17ea0e9110b5a9ab665b2d8a03
AigulKh <aigul.kh017@gmail.com>
2019-09-13 22:46:52 +0300

    fixed main

--- Code_generate/src/main.kt
+++ Code_generate/src/main.kt
@@ -1,4 +1,5 @@
 import com.example.Server
+import java.io.File
 
 //import com.example.Generator
 //import com.example.ProgramParameters
@@ -12,14 +13,18 @@ fun main(args: Array<String>) {
 //    val args_: MutableList<String> = mutableListOf()
 //    args_.addAll(args[0].split(' '))
 //    if (!args_.isEmpty()) {
-//        if ((parseInt(args_[0]) == 1 && args_.size >= 8) || ((parseInt(args_[0]) == 3 || parseInt(args_[0]) == 6) && args_.size == 7)
-//            || ((parseInt(args_[0]) == 2 || parseInt(args_[0]) == 4 || parseInt(args_[0]) == 5) && args_.size == 6)) {
+//        if ((parseInt(args_[0]) == 1 && args_.size >= 8) || (parseInt(args_[0]) == 3 && args_.size == 7)
+//            || ((parseInt(args_[0]) == 2 || parseInt(args_[0]) == 4 || parseInt(args_[0]) == 5 || parseInt(args_[0]) == 6) && args_.size == 6)
+//            || (parseInt(args_[0]) == 7 && args_.size == 11)) {
 //            val parameters = ProgramParameters(args_)
 //            val generator = Generator(parameters)
+//
 //            generator.programGenerate()
+//            generator.runtime()
 //        }
 //        else println(ERROR_TEXT)
 //    }
 //    else println(ERROR_TEXT)
-
 }
+
+fun readFile(fileName: String) = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
