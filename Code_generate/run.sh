cb9b5400112893439c2cec5a11f0ee6753053de3
Aigul Khafizova <aigul.kh017@gmail.com>
2020-06-03 16:46:39 +0300

    Update run.sh

--- Code_generate/run.sh
+++ Code_generate/run.sh
@@ -3,7 +3,3 @@ set -e
 clang-format -i program.c
 gcc program.c -o program
 timeout 0.1s ./program > program_result.txt
-
-#clang-format -i Main.java
-#javac Main.java
-#java -classpath . Main > program_result.txt
