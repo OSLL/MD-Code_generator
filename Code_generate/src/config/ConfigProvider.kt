671ed26f189762bca85144997be8105c475e148c
Dmitrii <mandelshtamd@yandex.ru>
2021-11-10 09:16:59 +0300

    Add config file and its handler

---
+++ Code_generate/src/config/ConfigProvider.kt
@@ -0,0 +1,23 @@
+package com.example.config
+
+import java.io.FileInputStream
+import java.util.Properties
+
+object ConfigProvider {
+    private val configFile = FileInputStream("Code_generate/resources/config.properties")
+    private val appProps = Properties()
+
+    init {
+        appProps.load(configFile)
+    }
+
+    val dbUrl: String by lazy { appProps.getProperty("db.url") }
+    val dbUser: String by lazy { appProps.getProperty("db.user") }
+    val dbPassword: String by lazy { appProps.getProperty("db.password") }
+    val dbDriver: String by lazy { appProps.getProperty("db.driver") }
+
+    val debugUrl: String by lazy { appProps.getProperty("debug.url") }
+
+    val adminPassword: String by lazy { appProps.getProperty("admin.password") }
+    val adminUser: String by lazy { appProps.getProperty("admin.user") }
+}
