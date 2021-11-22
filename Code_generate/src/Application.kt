9c4f5f2c4706e24a8a7fcca607ea2e66ae833c3e
Dmitrii <mandelshtamd@yandex.ru>
2021-11-22 11:08:12 +0300

    add config info logging

--- Code_generate/src/Application.kt
+++ Code_generate/src/Application.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.example.config.ConfigProvider
 import io.ktor.application.*
 import io.ktor.features.*
 import org.slf4j.Logger
@@ -7,6 +8,8 @@ import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
     val log: Logger = LoggerFactory.getLogger("jsonLogger")
+    log.info("debugUrl = ${ConfigProvider.debugUrl}")
+    log.info("dbUrl = ${ConfigProvider.dbUrl}")
     io.ktor.server.jetty.EngineMain.main(args)
 }
 
