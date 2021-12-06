aa59e0f73249e113d84d01fd77a9fcc93130441f
Nikita Bosov <neckbosov@gmail.com>
2021-12-07 00:28:58 +0300

    Fix parameters parsing

--- Code_generate/src/utils.kt
+++ Code_generate/src/utils.kt
@@ -9,27 +9,22 @@ val jsonParser = Json {
 
 fun parseGenerationParameters(queryParameters: Map<String, List<String>>) = buildJsonObject {
     for (entry in queryParameters) {
-        if (entry.value.size > 1) {
-            if (entry.key == "operations") {
-                put(entry.key, JsonArray(entry.value.map { JsonPrimitive(it) }))
-            } else {
-                throw ParametersParseError("${entry.key} should not be an array")
+        val value = entry.value[0]
+        when (entry.key) {
+            "operations" -> {
+                put(entry.key, JsonArray(entry.value[0].split(',').map { JsonPrimitive(it) }))
             }
-        } else {
-            val value = entry.value[0]
-            when (entry.key) {
-                "rand_seed" -> {
-                    put(entry.key, value)
-                }
-                "redefinition_var" -> {
-                    put(entry.key, value == "1")
-                }
-                else -> {
-                    put(
-                        entry.key,
-                        value.toIntOrNull() ?: throw ParametersParseError("${entry.key} should be an integer value")
-                    )
-                }
+            "rand_seed" -> {
+                put(entry.key, value)
+            }
+            "redefinition_var" -> {
+                put(entry.key, value == "1")
+            }
+            else -> {
+                put(
+                    entry.key,
+                    value.toIntOrNull() ?: throw ParametersParseError("${entry.key} should be an integer value")
+                )
             }
         }
     }
